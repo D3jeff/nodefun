52652ded4a52a5905234462c5ecb134c9fa3adea
D3jeff <35748848+D3jeff@users.noreply.github.com>
2018-11-13 17:33:00 -0700

    Practicing functions

---
+++ function.js
@@ -0,0 +1,17 @@
+function greet(name,age) {
+  console.log('Goodbye ' + name + ' who is ' + age + ' years old');
+ }
+
+greet('Cavan',10);
+greet('Luke',10);
+greet('Wesley',9);
+greet('Chris',9);
+greet('Ella',10);
+
+function crazy(a,b) {
+  return a - b;
+}
+
+console.log(crazy(1,2));
+
+greet(crazy(205749,2573904),crazy(2,2));
